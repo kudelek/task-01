e5f82e10b5c53571fbf0aa3de0e928207c9212e9
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2021-10-13 15:53:58 +0200

    cleanup made

--- task-01/src/containers/Form.js
+++ task-01/src/containers/Form.js
@@ -1,27 +1,7 @@
-import React, { useState, useReducer, useEffect } from "react";
-
-function reducer(state, action){
-    console.log("Reducer used.");
-    switch (action.type) {
-      case "inputChange": {
-        //const newValue = action.payload;
-        console.log("Current state: ", state);
-        const newState = {...state, [ev.target.name]: ev.target.value}
-        console.log("Returning new state: ", newState);
-        return newState;
-      }
-      default: {
-        return {...state};
-      }
-    }
-    
-}
+import React, { useState, useEffect } from "react";
 
 export default function Form(){
-  const [personData, setPersonData] = useState({
-  });
-  const [state, dispatch] = useReducer(reducer, personData);
-  console.log("reducer state: ", state);
+  const [personData, setPersonData] = useState({});
   const [isloading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -33,15 +13,9 @@ export default function Form(){
       }
     })
     .then(data => {
-      console.log("data: ", data);
       if(data){
-        console.log("Person data before set: ", personData);
-        console.log("data to be set: ", data);
         setPersonData(data[0]);
-        console.log("Person data set");
       }
-
-      console.log("Person data: ", personData);
     })
     .catch(error => {
       console.log("Error fetching data: ",error);
@@ -54,10 +28,6 @@ export default function Form(){
   function handleSubmit(ev){
     ev.preventDefault();
 
-    console.log("submitting person data: ", personData);
-    console.log(Array(personData));
-    console.log(JSON.stringify(personData));
-
     const requestOptions = {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
@@ -66,27 +36,12 @@ export default function Form(){
     fetch('http://localhost:3000/user/1', requestOptions)
     .then(response => {
       response.json();
-      console.log("response: ", response);
-      console.log("person: ", personData);
     });
   }
 
   function handleChange(ev){
-    console.log("person data before change: ", personData)
-    //setPersonData({...personData, [ev.target.name]: ev.target.value});
-    console.log("change made in: " + ev.target.name + ". Value: " + ev.target.value);
-    console.log("Updated person data: ", personData);
+    setPersonData({...personData, [ev.target.name]: ev.target.value});}
 
-    const action = {
-      type: "inputChange",
-      payload: ev,
-    }
-
-    dispatch(action);
-
-  }
-
-  
     return ( isloading ? <div>Loading...</div> : (
       <form onSubmit={handleSubmit}>
         <label>
@@ -109,7 +64,6 @@ export default function Form(){
       </form>
     )
     )
-  
 
 
 }
