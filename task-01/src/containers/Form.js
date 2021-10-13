8df9dcdbfa2e2453c918bb77048a83d0c80d809e
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2021-10-13 15:49:37 +0200

    useReducer finally working!!!

--- task-01/src/containers/Form.js
+++ task-01/src/containers/Form.js
@@ -1,47 +1,43 @@
 import React, { useState, useReducer, useEffect } from "react";
 
 function reducer(state, action){
-    console.log("Reducer used.");
     switch (action.type) {
-      case "inputChange": {
-        //const newValue = action.payload;
-        console.log("Current state: ", state);
-        const newState = {...state, [ev.target.name]: ev.target.value}
-        console.log("Returning new state: ", newState);
+      case "fieldChange": {
+        const newState = {...state, [String(action.payload.name)]: String(action.payload.value)}
         return newState;
+      }
+
+      case "setData": {
+          const newState = action.payload;
+          return newState;
       }
       default: {
         return {...state};
       }
     }
-    
 }
 
 export default function Form(){
-  const [personData, setPersonData] = useState({
-  });
-  const [state, dispatch] = useReducer(reducer, personData);
-  console.log("reducer state: ", state);
+  const [userData, dispatch] = useReducer(reducer, {});
   const [isloading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:3000/user')
+      loadData();
+  },[])
+
+  async function loadData(){
+    await fetch('http://localhost:3000/user')
     .then(response =>{
       if(response.ok) {
-        console.log("GET response: ", response);
         return response.json();
       }
     })
     .then(data => {
       console.log("data: ", data);
       if(data){
-        console.log("Person data before set: ", personData);
-        console.log("data to be set: ", data);
-        setPersonData(data[0]);
-        console.log("Person data set");
+        let d = data[0];
+        dispatch({type: "setData", payload: d});
       }
-
-      console.log("Person data: ", personData);
     })
     .catch(error => {
       console.log("Error fetching data: ",error);
@@ -49,64 +45,58 @@ export default function Form(){
     .finally(() => {
       setIsLoading(false);
     });
-  },[])
+  }
 
   function handleSubmit(ev){
     ev.preventDefault();
 
-    console.log("submitting person data: ", personData);
-    console.log(Array(personData));
-    console.log(JSON.stringify(personData));
+    console.log(JSON.stringify(userData));
 
     const requestOptions = {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      body: Array(JSON.stringify(personData))
+      body: Array(JSON.stringify(userData))
     };
     fetch('http://localhost:3000/user/1', requestOptions)
     .then(response => {
       response.json();
-      console.log("response: ", response);
-      console.log("person: ", personData);
     });
   }
 
   function handleChange(ev){
-    console.log("person data before change: ", personData)
-    //setPersonData({...personData, [ev.target.name]: ev.target.value});
-    console.log("change made in: " + ev.target.name + ". Value: " + ev.target.value);
-    console.log("Updated person data: ", personData);
-
+    console.log("change made in: " + ev.target.id + ". Value: " + ev.target.value);
     const action = {
-      type: "inputChange",
-      payload: ev,
+      type: "fieldChange",
+      payload: {name: [ev.target.id], value: [ev.target.value]},
     }
-
     dispatch(action);
 
   }
-
   
     return ( isloading ? <div>Loading...</div> : (
-      <form onSubmit={handleSubmit}>
-        <label>
-            First Name:   
-            <input type="text" name="firstName" defaultValue={personData.firstName} onChange={handleChange}/>
-        </label><br />
-        <label>
-            Last Name:   
-            <input type="text" name="lastName" defaultValue={personData.lastName} onChange={handleChange}/>
-        </label><br />
-        <label>
-            Birth Date:    
-            <input type="text" name="birthDate" defaultValue={personData.birthDate} onChange={handleChange}/>
-        </label><br />
-        <label>
-            Eye Color:     
-            <input type="text" name="eyeColor" defaultValue={personData.eyeColor} onChange={handleChange}/>
-        </label><br />
-        <input type="submit" value="submit"/>
-      </form>
+      <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+                First Name:   
+                <input type="text" id="firstName" defaultValue={userData.firstName} onChange={handleChange}/>
+            </label><br />
+            <label>
+                Last Name:   
+                <input type="text" id="lastName" defaultValue={userData.lastName} onChange={handleChange}/>
+            </label><br />
+            <label>
+                Birth Date:    
+                <input type="text" id="birthDate" defaultValue={userData.birthDate} onChange={handleChange}/>
+            </label><br />
+            <label>
+                Eye Color:     
+                <input type="text" id="eyeColor" defaultValue={userData.eyeColor} onChange={handleChange}/>
+            </label><br />
+            <input type="submit" value="submit"/><br />
+            <p>{}</p>
+        </form>
+            <button onClick={() => console.log("userdata from click",userData)}>log userData</button>
+      </div>
     )
     )
   
