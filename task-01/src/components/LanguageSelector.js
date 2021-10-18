6f55145e145186d646de56e63839b75232c80039
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2021-10-18 11:06:45 +0200

    cosmetic changes

--- task-01/src/components/LanguageSelector.js
+++ task-01/src/components/LanguageSelector.js
@@ -1,19 +1,23 @@
 import React, { useContext } from "react";
-
+import { Text } from "../libs/language";
 import { LanguageContext } from "../libs/language";
 import { languageOptions } from "../languages/dictionaryList";
+import './LanguageSelector.css';
 
-export default function LanguageSelector(){
+export default function LanguageSelector(props){
     const { userLanguage, userLanguageChange } = useContext(LanguageContext)
 
     const handleLanguageChange = e => userLanguageChange(e.target.value);
 
     return (
-        <select
-            onChange={handleLanguageChange}
-            value={userLanguage}
-            >
+        <div {...props}>
+            <div className="langtext"><Text tid="language"/></div>
+            <select
+                onChange={handleLanguageChange}
+                value={userLanguage}
+                >
                 {Object.entries(languageOptions).map(([id, name]) => <option key={id} value={id}>{name}</option>)}
             </select>
+        </div>
     );
 };
