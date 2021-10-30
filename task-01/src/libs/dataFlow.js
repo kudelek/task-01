c04d883671440654497ffce2e8ef005fb325f31f
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2021-10-30 15:27:05 +0200

    created fetchAPI, separated formAPI, next will have to separate contactAPI

--- task-01/src/libs/dataFlow.js
+++ task-01/src/libs/dataFlow.js
@@ -1,40 +1,41 @@
-export async function loadData(setIsLoading){
-  const response = await fetch('http://localhost:3000/user')
-  .then(response =>{
-    if(response.ok) {
-      return response.json();
-    }
-  })
-  .then(data => {
-    if(data){
-      let d = data[0];
-      return d;
-    }
-  })
-  .catch(error => {
-    console.log("Error fetching data: ",error);
-  })
-  .finally(() => {
-    setIsLoading(false);
-  });
-  return response;
-}
+// export async function loadData(setIsLoading){
+//   const response = await fetch('http://localhost:3000/user')
+//   .then(response =>{
+//     if(response.ok) {
+//       return response.json();
+//     }
+//   })
+//   .then(data => {
+//     if(data){
+//       let d = data[0];
+//       return d;
+//     }
+//   })
+//   .catch(error => {
+//     console.log("Error fetching data: ",error);
+//   })
+//   .finally(() => {
+//     setIsLoading(false);
+//   });
+//   return response[0];
+// }
 
-export async function submitData(e, userData){
-  e.preventDefault();
+// export async function submitData(e, userData){
+//   e.preventDefault();
   
-  const requestOptions = {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: Array(JSON.stringify(userData))
-  };
+//   const requestOptions = {
+//     method: 'PUT',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: Array(JSON.stringify(userData))
+//   };
   
-  fetch('http://localhost:3000/user/1', requestOptions)
-  .then(response => {
-    return response.json();
-  });
+//   fetch('http://localhost:3000/user/1', requestOptions)
+//   .then(response => {
+//     console.log("response uD: ", response);
+//     return response.json();
+//   });
 
-}
+// }
 
 export async function submitContact(e, contactData){
   e.preventDefault();
